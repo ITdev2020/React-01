a80f4abb23c5201aab8cf9cdea37aa44dfbc252a
ITdev2020 <igoris.talaiko.darbui@gmail.com>
2021-02-11 12:47:59 +0200

    13.

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+	return (
+		<header className="header">
+			<img src="https://www.logodesign.net/logo/line-art-triangle-forming-letter-a-shape-5087ld.png"></img>
+		</header>
+	);
+};
+
+export default Header;
