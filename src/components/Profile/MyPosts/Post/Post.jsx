a5bc45bf498745b5fd6b1113ee2e7cded265cc13
ITdev2020 <igoris.talaiko.darbui@gmail.com>
2021-02-19 07:53:39 +0200

    25

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -6,9 +6,9 @@ const Post = (props) => {
 	return (
 		<div className={s.item}>
 			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDkvFCLSMbUU6Bqb1m-0y3LPAQ7_Gcs-PNZw&usqp=CAU"></img>
-			post 1+{props.message}
+			`post1: {props.message}`
 			<div>
-				<span>Like {props.likeCount}</span>
+				<span>Like {props.likesCount}</span>
 			</div>
 		</div>
 	);
