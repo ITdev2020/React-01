c241c64283f0d042cffc035696bcd9f8e629cf31
ITdev2020 <igoris.talaiko.darbui@gmail.com>
2021-02-26 16:18:06 +0200

    30

--- src/App.js
+++ src/App.js
@@ -7,39 +7,33 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import {BrowserRouter, Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 
 function App(props) {
 
-
-
   return (
-    <BrowserRouter>
-      <div>
-        <img src={logo} className="App-logo" alt="logo"/>
-        <div className="app-wrapper">
-          <Header/>
-          <Navbar/>
-          <div className='app-wrapper-content'>
+    <div>
+      <img src={logo} className="App-logo" alt="logo"/>
+      <div className="app-wrapper">
+        <Header/>
+        <Navbar/>
+        <div className='app-wrapper-content'>
 
-            <Route path='/profile' render={
-              () => <Profile state={props.state.profilePage}/>
-            }/>
+          <Route path='/profile' render={
+            () => <Profile state={props.state.profilePage}/>
+          }/>
 
+          <Route path='/dialogs' render={
+            () => <Dialogs state={props.state.dialogsPage}/>
+          }/>
 
-            <Route path='/dialogs' render={
-              () => <Dialogs state={props.state.dialogsPage}/>
-            }/>
-
-
-            <Route path='/news' component={News}/>
-            <Route path='/music' component={Music}/>
-            <Route path='/settings' component={Settings}/>
-          </div>
+          <Route path='/news' component={News}/>
+          <Route path='/music' component={Music}/>
+          <Route path='/settings' component={Settings}/>
         </div>
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
