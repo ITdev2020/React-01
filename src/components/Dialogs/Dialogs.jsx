61df2b4590c95fdb78c7d5eaf6e838477f869149
ITdev2020 <igoris.talaiko.darbui@gmail.com>
2021-02-26 17:44:30 +0200

    31

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,44 +5,6 @@ import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
-    // let dialogs = [
-    //   {id: 1, name: 'Dimych'},
-    //   {id: 2, name: 'Andrey'},
-    //   {id: 3, name: 'Sveta'},
-    //   {id: 4, name: 'Sasha'},
-    //   {id: 5, name: 'Viktor'},
-    //   {id: 6, name: 'Valera'}
-    // ]
-    //
-    // let messages = [
-    //   {id: 1, message: 'Hi'},
-    //   {id: 2, message: 'How is your?'},
-    //   {id: 3, message: 'Yahoo'},
-    //   {id: 4, message: 'Yahoo'},
-    //   {id: 5, message: 'Yahoo'},
-    //   {id: 6, message: 'By'}
-    // ]
-    //
-    //
-    // let dialogsElements = dialogs.map(
-    //   d => <DialogItem id={d.id} name={d.name}/>
-    // )
-
-  /*
-[
-  <DialogItem id={dialogs[0].id} name={dialogs[0].name}/>,
-  <DialogItem id={dialogs[1].id} name={dialogs[1].name}/>,
-  <DialogItem id={dialogs[2].id} name={dialogs[2].name}/>,
-  <DialogItem id={dialogs[3].id} name={dialogs[3].name}/>,
-  <DialogItem id={dialogs[4].id} name={dialogs[4].name}/>,
-  <DialogItem id={dialogs[5].id} name={dialogs[5].name}/>
-]
-*/
-
-    // let messagesElements = messages.map(
-    //   m => <Message message={m.message}/>
-    // )
-
   let dialogsElements = props.state.dialogs.map(
     d => <DialogItem id={d.id} name={d.name}/>
   )
@@ -51,38 +13,21 @@ const Dialogs = (props) => {
     m => <Message message={m.message}/>
   )
 
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
 
-    return (
-      <div className={s.dialogs}>
-        <div className={s.dialogsItems}>
+        {dialogsElements}
 
-
-          {dialogsElements}
-
-
-          {/*<DialogItem id={dialogs[0].id} name={dialogs[0].name}/>*/}
-          {/*<DialogItem id={dialogs[1].id} name={dialogs[1].name}/>*/}
-          {/*<DialogItem id={dialogs[2].id} name={dialogs[2].name}/>*/}
-          {/*<DialogItem id={dialogs[3].id} name={dialogs[3].name}/>*/}
-          {/*<DialogItem id={dialogs[4].id} name={dialogs[4].name}/>*/}
-          {/*<DialogItem id={dialogs[5].id} name={dialogs[5].name}/>*/}
-        </div>
-        <div className={s.messages}>
-
-
-          {messagesElements}
-
-
-          {/*<Message message={messages[0].message}/>*/}
-          {/*<Message message={messages[1].message}/>*/}
-          {/*<Message message={messages[2].message}/>*/}
-          {/*<Message message={messages[3].message}/>*/}
-          {/*<Message message={messages[4].message}/>*/}
-          {/*<Message message={messages[5].message}/>*/}
-        </div>
       </div>
-    );
-  }
-;
+      <div className={s.messages}>
+
+        {messagesElements}
+
+      </div>
+    </div>
+  );
+
+};
 
 export default Dialogs;
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,34 +4,16 @@ import React from "react";
 
 const MyPosts = (props) => {
 
-
-  // console.log(props.message)
-
-
   let postsElements = props.posts.map(
-// p => <Post message={p.message} likesCount={p.likesCount}/>
     p => <Post message={p.message} likesCount={p.likesCount}/>
   )
 
+  let newPostElement = React.createRef();
 
-  // let posts = [
-  //   {id: 1, message: 'Hi, how are you?', likesCount: 53},
-  //   {id: 2, message: 'It\'s my first post', likesCount: 3},
-  //   {id: 3, message: 'How is life?', likesCount: 6},
-  //   {id: 4, message: 'Nothing', likesCount: 23},
-  //   {id: 5, message: 'By', likesCount: 0},
-  // ]
-
-
-  /*
-let postsElements = props.postsArr.map(
-  // p => <Post message={p.message} likesCount={p.likesCount}/>
-  p => <Post message={p.message} likesCount={p.likesCount}/>
-)
-*/
-
-
-
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  }
 
   return (
     <div className={s.postsBlock}>
@@ -39,34 +21,23 @@ let postsElements = props.postsArr.map(
       <div>{props.propsProperties}</div>
       <div>
         <div>
-          <textarea>New post</textarea>
+
+          <textarea ref={newPostElement}>New post</textarea>
+
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={addPost}>Add post</button>
           <button>Remove</button>
         </div>
       </div>
       <div className={s.posts}>
 
-        {/*{postsElements}*/}
-
         {postsElements}
-
-        {/*<Post message={posts[0].message} likesCount={posts[0].likesCount}/>*/}
-
-
-
-        {/*<Post message={posts[0].message} likesCount={posts[0].likesCount}/>*/}
-        {/*<Post message={posts[1].message} likesCount={posts[1].likesCount}/>*/}
-        {/*<Post message={posts[2].message} likesCount={posts[2].likesCount}/>*/}
-        {/*<Post message={posts[3].message} likesCount={posts[3].likesCount}/>*/}
-        {/*<Post message={posts[4].message} likesCount={posts[4].likesCount}/>*/}
-
-
 
       </div>
     </div>
   );
+
 };
 
 export default MyPosts;
