cd9c8f122a5be5a3f9fe9c21a49da05e45512165
ITdev2020 <igoris.talaiko.darbui@gmail.com>
2021-03-02 15:21:39 +0200

    34a

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import s from './Header.module.css';
 
 const Header = () => {
 	return (
+		//<header /> component from React
 		<header className={s.header}>
 			<img src="https://www.logodesign.net/logo/line-art-triangle-forming-letter-a-shape-5087ld.png"></img>
 		</header>
