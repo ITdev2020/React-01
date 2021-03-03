b6907492bd9252558d4af80b69f77d1038057b57
ITdev2020 <igoris.talaiko.darbui@gmail.com>
2021-03-03 18:09:54 +0200

    39

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,7 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
+
 let store = {
   _state: {
 
@@ -46,7 +50,7 @@ let store = {
 
   //action - object. dispatch - 'send'.
   dispatch(action) {  // { type: 'ADD-POST' }, for example.
-    if (action.type === 'ADD-POST') {
+    if (action.type === ADD_POST) {
       let newPost = {
         id: 6,
         message: this._state.profilePage.newPostText,
@@ -55,12 +59,31 @@ let store = {
       this._state.profilePage.posts.push(newPost)
       this._state.profilePage.newPostText = ''
       this._callSubscriber(this._state)
-    } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
       this._state.profilePage.newPostText = action.newText
       this._callSubscriber(this._state)
     }
   }
 }
+
+export const addPostActionCreator = () => ({type: ADD_POST})
+// export const addPostActionCreator = () => {
+//   return {
+//     type: ADD_POST
+//   }
+// }
+
+export const updateNewPostTextActionCreator = (text) => ({
+  type: UPDATE_NEW_POST_TEXT, newText: text
+})
+// export const updateNewPostTextActionCreator = (text) => {
+//   return {
+//     type: UPDATE_NEW_POST_TEXT,
+//     newText: text
+//   }
+// }
+
+
 
 // API - application program interface
 
