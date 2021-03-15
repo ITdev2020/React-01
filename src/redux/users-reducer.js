f5d57aefc4aa88593bc18b9730c7a1796db07676
ITdev2020 <igoris.talaiko.darbui@gmail.com>
2021-03-15 18:32:21 +0200

    55

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,12 +1,15 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 
 let initialState = {
-  users: [
-
-  ]
+  users: [ ],
+  pageSize: 100,
+  totalUsersCount: 0,
+  currentPage: 2
 }
 
 // if state not defined (on start), then state = initialState.
@@ -50,8 +53,14 @@ const usersReducer = (state = initialState, action) => {
       }
 
     case SET_USERS:
-      return {...state, users: [...state.users, ...action.users]}
+      return {...state, users: action.users}
     // return { ...state, users: action.users }
+
+    case SET_CURRENT_PAGE:
+      return {...state, currentPage: action.currentPage}
+
+    case SET_TOTAL_USERS_COUNT:
+      return {...state, totalUsersCount: action.count}
 
     default:
       return state;
@@ -65,6 +74,11 @@ export const followAC = (userId) => ({type: FOLLOW, userId})
 export const unfollowAC = (userId) => ({type: UNFOLLOW, userId})
 // users - we give, maybe from server, later. And fill our state.
 export const setUsersAC = (users) => ({type: SET_USERS, users})
+//-if we create object and properties name is same this properties variable name (currentPage: currentPage),
+//    we do not need write properties name.
+// export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage: currentPage})
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
+export const setUsersTotalCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount})
 
 
 export default usersReducer;
