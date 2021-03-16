23d006764af872ac053dfd3e50c60f2b76c1cd03
ITdev2020 <igoris.talaiko.darbui@gmail.com>
2021-03-17 01:55:39 +0200

    62

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@
 import styles from "./users.module.css";
 import userPhoto from "../../assets/images/user.png";
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 let Users = (props) => {
   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -23,7 +24,6 @@ let Users = (props) => {
     </div>
     {
       props.users.map(u => <div key={u.id}>
-        {console.log(u)}
         <span>
           <div>
             <NavLink to={'/profile/' + u.id}>
@@ -34,10 +34,34 @@ let Users = (props) => {
             {/* ternary operator */}
             {u.followed
               ? <button onClick={() => {
-                props.unfollow(u.id)
+
+                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                  withCredentials: true,
+                  headers: {
+                    'API-KEY': '9ba50d04-a62e-4b79-81da-db22d5a24da9'
+                  }
+                })
+                  .then(response => {
+                    if (response.data.resultCode === 0) {
+                      props.unfollow(u.id);
+                    }
+                  });
+
               }}>Unfollow</button>
               : <button onClick={() => {
-                props.follow(u.id)
+
+                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                  withCredentials: true,
+                  headers: {
+                    'API-KEY': '9ba50d04-a62e-4b79-81da-db22d5a24da9'
+                  }
+                })
+                  .then(response => {
+                    if (response.data.resultCode === 0) {
+                      props.follow(u.id);
+                    }
+                  });
+
               }}>Follow</button>
             }
           </div>
